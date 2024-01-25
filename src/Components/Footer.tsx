73093d92249b4ae1f1f0b252cc76c906afc69e22
Tushar Bhatt <tusharbhatt0135@gmail.com>
2024-01-26 02:21:29 +0530

    ui

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,9 +1,10 @@
 import Socials from "./other/Socials";
+import { FaFireAlt } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col justify-center gap-4 p-7 bg-blue-800 text-white text-center mt-12">
-      <p className="text-2xl font-bold">Thankyou for the Visit 👋</p>
+    <footer className="flex flex-col justify-center gap-4 p-7 bg-black text-white text-center mt-12">
+      <p className="text-2xl font-bold flex justify-center items-center gap-2">Thankyou for the Visit <FaFireAlt className="text-yellow-400"/></p>
       <div className="flex flex-col gap-4 sm:flex-row justify-between items-center">
       <Socials />
       <p>&copy; 2024 Tushar Bhatt. All rights reserved.</p>
