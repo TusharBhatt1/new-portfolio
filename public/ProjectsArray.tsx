53e7ca642a74a508c2e2a4a2e1afa06855d33c30
Tushar Bhatt <tusharbhatt0135@gmail.com>
2024-02-06 15:16:30 +0530

    new project - genius

--- public/ProjectsArray.tsx
+++ public/ProjectsArray.tsx
@@ -1,15 +1,16 @@
-import airbnb from "../src/assets/Images/RentApp.png"
-import netflix from "../src/assets/Images/Netflix.png"
-import ecommerce from "../src/assets/Images/New-Ecommerce.png"
-import bestbuy from "../src/assets/Images/BestBuy.png"
-import sketch from "../src/assets/Images/Sketch.png"
+import airbnb from "../src/assets/Images/RentApp.png";
+import netflix from "../src/assets/Images/Netflix.png";
+import ecommerce from "../src/assets/Images/New-Ecommerce.png";
+import bestbuy from "../src/assets/Images/BestBuy.png";
+import sketch from "../src/assets/Images/Sketch.png";
+import genius from "../src/assets/Images/Screenshot (243).png";
 
 export interface projectType {
   name: string;
   tech: string;
   liveLink: string;
   gitLink: string;
-  image:string
+  image: string;
 }
 interface ProjectsType {
   fullStack: projectType[];
@@ -19,11 +20,18 @@ interface ProjectsType {
 export const projects: ProjectsType = {
   fullStack: [
     {
+      name: "Genius - AI Companion using OPENAI and KNIT.ai",
+      tech: "MongoDB , Express , Node , React , Typescript , Tailwind",
+      liveLink: "https://genius-mern-fe.vercel.app/",
+      gitLink: "https://github.com/TusharBhatt1/genius-mern",
+      image: genius,
+    },
+    {
       name: "Rent Application - where users can buy and rent properties",
       tech: "NextJS , MongoDB , Prisma , Typescript , React , Tailwind",
       liveLink: "https://rent-app-alpha.vercel.app/",
       gitLink: "https:github.com/TusharBhatt1/airbnb",
-      image:airbnb
+      image: airbnb,
     },
   ],
   frontEnd: [
@@ -32,30 +40,29 @@ export const projects: ProjectsType = {
       liveLink: "https://zocket-alpha.vercel.app/",
       tech: "React , Typescript , LocalStorage , ColorPicker , React Icons , Tailwind",
       gitLink: "https:github.com/TusharBhatt1/zocket",
-      image:sketch
+      image: sketch,
     },
     {
-        name: "Best Buy - Add to Wishlist + LRU Caching for recently viewed",
-        liveLink: "https://bestbuy-main.vercel.app/",
-        tech: "NextJS , Typescript, React , Zustand , React-hook-form , Yup , Tailwind",
-        gitLink: "https:github.com/TusharBhatt1/bestbuy",
-        image:bestbuy
-      },
+      name: "Best Buy - Add to Wishlist + LRU Caching for recently viewed",
+      liveLink: "https://bestbuy-main.vercel.app/",
+      tech: "NextJS , Typescript, React , Zustand , React-hook-form , Yup , Tailwind",
+      gitLink: "https:github.com/TusharBhatt1/bestbuy",
+      image: bestbuy,
+    },
     {
       name: "Netflix Clone - All in one entertainment Application",
       liveLink: "https://net-flix-7ewm.vercel.app/",
       tech: "React , Redux , CSS",
       gitLink: "https:github.com/TusharBhatt1/netflix",
-      image:netflix
+      image: netflix,
     },
-   
 
     {
       name: "React Ecommerce",
       liveLink: "https://new-ecommerce-five.vercel.app/",
       tech: "React , Redux , Responsive Design",
       gitLink: "https:github.com/TusharBhatt1/ecommerce",
-      image:ecommerce
+      image: ecommerce,
     },
   ],
 };
