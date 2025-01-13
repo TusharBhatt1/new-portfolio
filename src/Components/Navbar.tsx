effd3daa6d29e77305a23a50cc72b01d6f020a57
Tushar Bhatt <95581504+TusharBhatt1@users.noreply.github.com>
2025-01-13 23:36:53 +0530

    Update Navbar.tsx

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -13,7 +13,7 @@ export default function Navbar() {
           <p>Contact</p>
         </div>
         <div>
-          <a href="https://bit.ly/tb0135 " target="_blank" className="text-white bg-black p-2 px-3 rounded-lg hover:bg-blue-700">
+          <a href="https://bit.ly/tbv0" target="_blank" className="text-white bg-black p-2 px-3 rounded-lg hover:bg-blue-700">
             Resume
           </a>
         </div>
