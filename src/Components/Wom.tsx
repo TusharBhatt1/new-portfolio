667ee397568cfb9f3db53ab8722a90605fa34ca0
Tushar <tusharbhatt0135@gmail.com>
2025-04-14 00:29:30 +0530

    minor-change

--- src/Components/Wom.tsx
+++ src/Components/Wom.tsx
@@ -5,9 +5,8 @@ import wom2 from "@/assets/wom/wom2.png";
 import wom3 from "@/assets/wom/wom3.png";
 import wom4 from "@/assets/wom/wom4.png";
 import wom5 from "@/assets/wom/wom5.png";
-import wom6 from "@/assets/wom/image.png";
+import wom6 from "@/assets/wom/wom6.png";
 import wom7 from "@/assets/wom/wom7.png";
-
 
 export default function Wom() {
 	const woms = [
@@ -18,20 +17,15 @@ export default function Wom() {
 		{ src: wom5, title: "IC3 @Cal.com" },
 		{ src: wom6, title: "Director of Engineering @PocketFM" },
 		{ src: wom7, title: "Lead Front End Engineer @PocketFM" },
-
 	];
 
 	return (
 		<div>
 			<Heading title="Word of mouth" />
 			<Marquee speed={100} pauseOnHover>
-				{woms.map(({src, title}, index) => (
+				{woms.map(({ src, title }, index) => (
 					<div key={index} className="mx-2 cursor-pointer">
-						<img
-							src={src}
-							alt={`testimonial-${index + 1}`}
-							className="h-20"
-						/>
+						<img src={src} alt={`testimonial-${index + 1}`} className="h-20" />
 						<p className="text-center mt-1 text-slate-400">{title}</p>
 					</div>
 				))}
