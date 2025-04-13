4fdfb9e6142060e00f56cd44ff751030132a950a
Tushar <tusharbhatt0135@gmail.com>
2025-04-14 00:28:33 +0530

    more wom

--- src/Components/Wom.tsx
+++ src/Components/Wom.tsx
@@ -5,22 +5,34 @@ import wom2 from "@/assets/wom/wom2.png";
 import wom3 from "@/assets/wom/wom3.png";
 import wom4 from "@/assets/wom/wom4.png";
 import wom5 from "@/assets/wom/wom5.png";
+import wom6 from "@/assets/wom/image.png";
+import wom7 from "@/assets/wom/wom7.png";
 
 
 export default function Wom() {
-	const woms = [wom1, wom2, wom3, wom4, wom5];
+	const woms = [
+		{ src: wom1, title: "IC4 @Cal.com" },
+		{ src: wom2, title: "CEO @Cal.com" },
+		{ src: wom3, title: "Head of Engineering @Cal.com" },
+		{ src: wom4, title: "IC2 @Cal.com" },
+		{ src: wom5, title: "IC3 @Cal.com" },
+		{ src: wom6, title: "Director of Engineering @PocketFM" },
+		{ src: wom7, title: "Lead Front End Engineer @PocketFM" },
+
+	];
 
 	return (
 		<div>
 			<Heading title="Word of mouth" />
 			<Marquee speed={100} pauseOnHover>
-				{woms.map((wom, index) => (
-					<div key={index} className="mx-2">
+				{woms.map(({src, title}, index) => (
+					<div key={index} className="mx-2 cursor-pointer">
 						<img
-							src={wom}
+							src={src}
 							alt={`testimonial-${index + 1}`}
-							className="h-20 cursor-pointer"
+							className="h-20"
 						/>
+						<p className="text-center mt-1 text-slate-400">{title}</p>
 					</div>
 				))}
 			</Marquee>
