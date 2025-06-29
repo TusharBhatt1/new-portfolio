4e01169f69ec20c608ce24874b25efcf371d3648
Tushar Bhatt <95581504+TusharBhatt1@users.noreply.github.com>
2025-06-29 11:27:40 +0530

    Update Navbar.tsx

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -4,7 +4,7 @@ import Socials from "./other/Socials";
 export default function Navbar() {
   return (
     <div className="fixed w-full flex md:flex-row flex-col justify-between items-center p-4 border-b border-slate-800 z-10 bg-black font-bold">
-      <h1 className="md:block hidden">Hey 👋 I'm Tushar Bhatt  , <span className="text-sm">Full Stack Front End Engineer with ~ an year of experience</span></h1>
+      <h1 className="md:block hidden">Hey 👋 I'm Tushar Bhatt  , <span className="text-sm">Full Stack Front End Engineer with ~ a year of experience</span></h1>
       <Socials/>
     </div>
   );
